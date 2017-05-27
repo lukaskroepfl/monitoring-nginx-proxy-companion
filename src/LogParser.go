b3f32a86df20c23650c16fc9643acd1f867ecb6d
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-05-27 21:37:47 +0200

    parsing logline with regex

--- src/LogParser.go
+++ src/LogParser.go
@@ -1,48 +1,57 @@
 package main
 
 import (
-  "strings"
+  "regexp"
+  "strconv"
 )
 
 type ParsedLogLine struct {
   containerName string
-  host string
-  sourceIp string
-  timestamp string
-  requestType string
-  requestPath string
-  responseCode int
-  latency int
-  userAgent string
+  host          string
+  sourceIp      string
+  timestamp     string
+  requestMethod string
+  requestPath   string
+  httpVersion   string
+  httpStatus    int
+  bodyBytesSent int
+  httpReferer   string
+  userAgent     string
 }
 
-const CONTAINER_SPLIT = "|"
-
 func ParseLogLine(line string) ParsedLogLine {
+  var logLineParserRegex = regexp.MustCompile(`^(\S+) *\|\s+(\S+)\s+(\S+).+\[(.+)\]\s+"([^"]+)"\s+(\S+)\s+(\S+)\s+"([^"]+)"\s+"([^"]+)"`)
 
-  containerSplit := strings.Split(line, CONTAINER_SPLIT)
+  logLineParserRegexResult := logLineParserRegex.FindStringSubmatch(line)
 
-  containerNameWithSpaces := containerSplit[0]
-  containerName := strings.TrimRight(containerNameWithSpaces, " ")
+  containerName := logLineParserRegexResult[1]
+  host := logLineParserRegexResult[2]
+  remoteAddress := logLineParserRegexResult[3]
+  timestamp := logLineParserRegexResult[4]
+  httpRequest := logLineParserRegexResult[5]
+  var httpRequestRegex = regexp.MustCompile(`^(\S+)\s+(\S+)\s+(\S+)`)
+  httpRequestRegexResult := httpRequestRegex.FindStringSubmatch(httpRequest)
+  requestType := httpRequestRegexResult[1]
+  requestPath := httpRequestRegexResult[2]
+  httpVersion := httpRequestRegexResult[3]
 
-
-  logRest := containerSplit[1]
-
-  logRestParts := strings.Split(logRest, " ")
-
-  hostName := logRestParts[1]
-  sourceIp := logRestParts[2]
-
-  timeStart := strings.Index(logRest, "[") + 1
-  timeEnd := strings.Index(logRest, "]")
-
-  time := logRest[timeStart:timeEnd]
+  httpStatus, _ := strconv.Atoi(logLineParserRegexResult[6])
+  bodyBytesSent, _ := strconv.Atoi(logLineParserRegexResult[7])
+  httpReferer := logLineParserRegexResult[8]
+  userAgent := logLineParserRegexResult[9]
 
   parsedLogLine := ParsedLogLine{}
   parsedLogLine.containerName = containerName
-  parsedLogLine.host = hostName
-  parsedLogLine.sourceIp = sourceIp
-  parsedLogLine.timestamp = time
+  parsedLogLine.host = host
+  parsedLogLine.sourceIp = remoteAddress
+  parsedLogLine.timestamp = timestamp
+  parsedLogLine.requestMethod = requestType
+  parsedLogLine.requestPath = requestPath
+  parsedLogLine.httpVersion = httpVersion
+  parsedLogLine.httpStatus = httpStatus
+  parsedLogLine.bodyBytesSent = bodyBytesSent
+  parsedLogLine.httpReferer = httpReferer
+  parsedLogLine.userAgent = userAgent
 
   return parsedLogLine
 }
--- src/logparsing_test.go
+++ src/logparsing_test.go
@@ -6,12 +6,18 @@ import (
 )
 
 func TestParsesContainerName(t *testing.T) {
-  line := "nginx                                | kroepfl.io 54.89.89.135 - - [27/May/2017:13:00:57 +0000] \"GET / HTTP/1.1\" 200 2211 \"-\" \"Java/1.8.0_121\""
+  line := `nginx                                | blog.kroepfl.io 193.80.91.32 - - [27/May/2017:19:26:27 +0000] "GET /wp-content/uploads/2017/04/Untitled.png HTTP/1.1" 404 18000 "https://blog.kroepfl.io/" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"`
 
   parsedLogLine := ParseLogLine(line)
 
   assert.Equal(t, "nginx", parsedLogLine.containerName)
-  assert.Equal(t, "kroepfl.io", parsedLogLine.host)
-  assert.Equal(t, "54.89.89.135", parsedLogLine.sourceIp)
-  assert.Equal(t, "27/May/2017:13:00:57 +0000", parsedLogLine.timestamp)
+  assert.Equal(t, "blog.kroepfl.io", parsedLogLine.host)
+  assert.Equal(t, "193.80.91.32", parsedLogLine.sourceIp)
+  assert.Equal(t, "27/May/2017:19:26:27 +0000", parsedLogLine.timestamp)
+  assert.Equal(t, "GET", parsedLogLine.requestMethod)
+  assert.Equal(t, "/wp-content/uploads/2017/04/Untitled.png", parsedLogLine.requestPath)
+  assert.Equal(t, "HTTP/1.1", parsedLogLine.httpVersion)
+  assert.Equal(t, 404, parsedLogLine.httpStatus)
+  assert.Equal(t, 18000, parsedLogLine.bodyBytesSent)
+  assert.Equal(t, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", parsedLogLine.userAgent)
 }
