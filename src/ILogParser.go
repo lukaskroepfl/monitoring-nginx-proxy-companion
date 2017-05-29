210d9d57fd0b9efdc0665fce4c000f837f41fadf
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-05-29 21:53:09 +0200

    added parsed params to parsed log struct

--- src/ILogParser.go
+++ src/ILogParser.go
@@ -1,17 +1,23 @@
 package main
 
 type ParsedLogLine struct {
-  containerName string
-  host          string
-  sourceIp      string
-  timestamp     string
-  requestMethod string
-  requestPath   string
-  httpVersion   string
-  httpStatus    int
-  bodyBytesSent int
-  httpReferer   string
-  userAgent     string
+  containerName  string
+  host           string
+  sourceIp       string
+  timestamp      string
+  requestMethod  string
+  requestPath    string
+  httpVersion    string
+  httpStatus     int
+  bodyBytesSent  int
+  httpReferer    string
+  userAgent      string
+  browser        string
+  browserVersion string
+  os             string
+  country        string
+  city           string
+  latency        int
 }
 
 type ILogParser interface {
