3df67ce606b7d0e41df5d7c65aa60203a5a436b6
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-05-30 21:25:39 +0200

    added latest tag to image build script

--- build_image.sh
+++ build_image.sh
@@ -2,4 +2,4 @@
 
 ./build.sh
 
-docker build -t lukaskroepfl/monitoring-nginx-proxy-companion:`git describe` .
+docker build -t lukaskroepfl/monitoring-nginx-proxy-companion:`git describe` -t lukaskroepfl/monitoring-nginx-proxy-companion:latest .
