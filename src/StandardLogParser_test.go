b9440014218bd2b242d0605c2907331b0e1352a9
Lukas Kröpfl <lukas.kroepfl@gmail.com>
2017-06-03 19:24:05 +0200

    added test

--- src/StandardLogParser_test.go
+++ src/StandardLogParser_test.go
@@ -3,32 +3,37 @@ package main
 import (
   "testing"
   "github.com/stretchr/testify/assert"
+  "time"
 )
 
 func TestParsesSomeLogLines(t *testing.T) {
   line := `blog.kroepfl.io 193.80.91.32 - - [27/May/2017:19:26:27 +0000] "GET /wp-content/uploads/2017/04/Untitled.png HTTP/1.1" 404 18000 "https://blog.kroepfl.io/" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"`
 
-  mockUserAgentParser := MockUserAgentParser{}
+  userAgentParser := MssolaUserAgentParser{}
   mockIpLookupService := MockIpLookupService{}
 
   standardLogParser := StandardLogParser{
-    userAgentParser: mockUserAgentParser,
+    userAgentParser: userAgentParser,
     ipLookupService: mockIpLookupService,
   }
 
-  parsedLogLine, err := standardLogParser.Parse(line)
+  httpRequest, err := standardLogParser.Parse(line)
 
   if err != nil {
     t.Fail()
   }
 
-  assert.Equal(t, "blog.kroepfl.io", parsedLogLine.host)
-  assert.Equal(t, "193.80.91.32", parsedLogLine.sourceIp)
-  assert.Equal(t, "27/May/2017:19:26:27 +0000", parsedLogLine.timestamp)
-  assert.Equal(t, "GET", parsedLogLine.requestMethod)
-  assert.Equal(t, "/wp-content/uploads/2017/04/Untitled.png", parsedLogLine.requestPath)
-  assert.Equal(t, "HTTP/1.1", parsedLogLine.httpVersion)
-  assert.Equal(t, 404, parsedLogLine.httpStatus)
-  assert.Equal(t, 18000, parsedLogLine.bodyBytesSent)
-  assert.Equal(t, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", parsedLogLine.userAgent)
+  ti := time.Unix(1495913187, 0)
+
+  assert.Equal(t, "blog.kroepfl.io", httpRequest.host)
+  assert.Equal(t, "193.80.91.32", httpRequest.sourceIp)
+  assert.Equal(t, ti.Unix(), httpRequest.timestamp.Unix())
+  assert.Equal(t, "GET", httpRequest.requestMethod)
+  assert.Equal(t, "/wp-content/uploads/2017/04/Untitled.png", httpRequest.requestPath)
+  assert.Equal(t, "HTTP/1.1", httpRequest.httpVersion)
+  assert.Equal(t, 404, httpRequest.httpStatus)
+  assert.Equal(t, 18000, httpRequest.bodyBytesSent)
+  assert.Equal(t, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36", httpRequest.userAgent)
+  assert.Equal(t, "Chrome", httpRequest.browser)
+  assert.Equal(t, "58.0.3029.110", httpRequest.browserVersion)
 }
